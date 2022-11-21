5d7e8c6d710e6d4efc61ebe0a3171ad0c95120f3
nadyaOleynikova <kposhkanadya@yandex.ru>
2022-11-22 01:38:47 +0200

    Add Search word

---
+++ src/main/java/Client.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        int PORT = 8989;
+        String HOST = "127.0.0.1";
+        try (
+                Socket socket = new Socket(HOST, PORT);
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            Scanner in = new Scanner(System.in);
+            System.out.println(bufferedReader.readLine());
+            String word = in.nextLine();
+            printWriter.println(word);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,27 @@
-import java.io.File;
-import java.util.Arrays;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
         System.out.println(engine.search("бизнес"));
-
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(8989);) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+                    String word = bufferedReader.readLine();
+                    printWriter.println(engine.search(word));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 }
+
+
+
+
